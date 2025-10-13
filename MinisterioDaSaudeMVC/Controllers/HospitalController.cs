1bd2050832631deeba3457a260bed37f4f9aaf6f
MAURICIO SHIGUEMITSU KAMADO IKEDA <mauricioikeda17@gmail.com>
2025-10-13 18:01:32 -0300

    Fazendo o MVC do ministerio da saude

---
+++ MinisterioDaSaudeMVC/Controllers/HospitalController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MinisterioDaSaudeMVC.Controllers
+{
+    public class HospitalController : Controller
+    {
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public IActionResult Consulta()
+        {
+            return View();
+        }
+    }
+}
---
+++ MinisterioDaSaudeMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MinisterioDaSaudeMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
