7425700773d2bc4b1ac788c9ff5da2c4b77fb115
MAURICIO SHIGUEMITSU KAMADO IKEDA <mauricioikeda17@gmail.com>
2025-10-24 15:30:33 -0300

    Criando novo sistema do ministerio da saude do zero, ja adicionando pagina dinamica de cadastro de hospitais

---
+++ Atualizado/MinisterioDaSaudeSistema/Pages/Hospital/Cadastro.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MinisterioDaSaudeSistema.Pages.Hospital;
+
+public class Cadastro : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+    
+    public IActionResult OnGetTipoCadastro(string tipoCadastro)
+    {
+        switch (tipoCadastro)
+        {
+            case "instituicao":
+                return Partial("_CadastrarHospitalInstituicao");
+            case "unidade":
+                return Partial("_CadastrarHospitalUnidade");
+            default:
+                return Page();
+        }
+    }
+}
