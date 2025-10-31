512b61dfdb9c53a2823681244a02194261a52b41
MAURICIO SHIGUEMITSU KAMADO IKEDA <mauricioikeda17@gmail.com>
2025-10-31 13:52:44 -0300

    Resolvendo enums

--- Atualizado/HospitalzinhoSistema/models/paciente.ts
+++ Atualizado/HospitalzinhoSistema/models/paciente.ts
@@ -1,40 +1,45 @@
-export enum PacienteGenero {
-  Masculino = 1,
-  Feminino = 2,
-  Outro = 3
-}
+export const PacienteGenero = {
+  Masculino: 1,
+  Feminino: 2,
+  Outro: 3
+} as const;
 
-export enum PacienteEscolaridade {
-  Analfabeto = 1,
-  FundamentalIncompleto = 2,
-  FundamentalCompleto = 3,
-  MedioIncompleto = 4,
-  MedioCompleto = 5,
-  SuperiorIncompleto = 6,
-  SuperiorCompleto = 7,
-  PosGraduacao = 8,
-  NaoSeAplica = 9
-}
+export type PacienteGeneroType = typeof PacienteGenero[keyof typeof PacienteGenero];
 
-export enum PacienteEtinia
-{
-    Branca = 1,
-    Preta = 2,
-    Parda = 3,
-    Amarela = 4,
-    Indigena = 5
-}
+export const PacienteEscolaridade = {
+  Analfabeto: 1,
+  FundamentalIncompleto: 2,
+  FundamentalCompleto: 3,
+  MedioIncompleto: 4,
+  MedioCompleto: 5,
+  SuperiorIncompleto: 6,
+  SuperiorCompleto: 7,
+  PosGraduacao: 8,
+  NaoSeAplica: 9
+} as const;
+
+export type PacienteEscolaridadeType = typeof PacienteEscolaridade[keyof typeof PacienteEscolaridade];
+
+export const PacienteEtinia = {
+  Branca: 1,
+  Preta: 2,
+  Parda: 3,
+  Amarela: 4,
+  Indigena: 5
+} as const;
+
+export type PacienteEtiniaType = typeof PacienteEtinia[keyof typeof PacienteEtinia];
 
 export interface Paciente {
     nome: string;
     cns: string;
     cpf: string;
     dataNascimento?: string;
-    sexo?: PacienteGenero;
-    raca?: PacienteEtinia;
+    sexo?: PacienteGeneroType;
+    raca?: PacienteEtiniaType;
     nacionalidade?: string;
     naturalidade?: string;
-    escolaridade?: PacienteEscolaridade;
+    escolaridade?: PacienteEscolaridadeType;
     nomeMae?: string;
     cpfMae?: string;
     nomePai?: string;
