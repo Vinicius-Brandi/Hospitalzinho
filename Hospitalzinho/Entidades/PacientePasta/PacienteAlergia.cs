5563671a6234dd801b95f1f160e2e33552385c84
Vinícius Estevão <vinicius.estevao@interfocus.com.br>
2025-10-31 09:48:25 -0300

    Adicionando Paciente Alergia

---
+++ Hospitalzinho/Entidades/PacientePasta/PacienteAlergia.cs
@@ -0,0 +1,18 @@
+﻿using FGB.Entidades;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Hospitalzinho.Entidades
+{
+    public class PacienteAlergia : EntidadeBase
+    {
+        [Required]
+        public virtual PacienteProntuario Prontuario { get; set; }
+
+        [Required]
+        public virtual Alergia Alergia { get; set; }
+
+        public virtual string? Observacao { get; set; }
+    }
+
+}
--- Hospitalzinho/Entidades/PacientePasta/PacienteProntuario.cs
+++ Hospitalzinho/Entidades/PacientePasta/PacienteProntuario.cs
@@ -9,14 +9,12 @@ namespace Hospitalzinho.Entidades
         public virtual Paciente Paciente { get; set; }
         [Required]
         public virtual TipoSanguineo TipoSangue { get; set; }
-        public virtual IList<Alergia> Alergias { get; set; } = new List<Alergia>();
+        public virtual IList<PacienteAlergia> Alergias { get; set; } = new List<PacienteAlergia>();
         public virtual IList<PacienteDoencaCronica> DoencasCronicas { get; set; } = new List<PacienteDoencaCronica>();
 
         public virtual IList<PacienteMedicacao> MedicacoesContinuas { get; set; } = new List<PacienteMedicacao>();
         public virtual IList<PacienteCirurgia> Cirurgias { get; set; } = new List<PacienteCirurgia>();
         public virtual IList<PacienteVacinacao> Vacinacoes { get; set; } = new List<PacienteVacinacao>();
-        public virtual DateTime? DataAbertura { get; set; }
-        public virtual DateTime? UltimaAtualizacao { get; set; }
 
         public virtual IList<PacienteConsulta> Consultas { get; set; } = new List<PacienteConsulta>();
         public virtual IList<PacienteInternacao> Internacoes { get; set; } = new List<PacienteInternacao>();
